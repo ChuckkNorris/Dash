c61cd63dfd34e4febb3482c075b6c12f08990b55
Levi Fuller <lfuller941@gmail.com>
2016-02-25 22:19:17 -0700

    Init

---
+++ Dash/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Dash.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Dash
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+          
+        }
+
+//        private void Set() {
+//            this.txtToDos.Document.SetText(Windows.UI.Text.TextSetOptions.FormatRtf, @"Dash
+//•	Set Lunch w / Oliver->Click Opens calendar
+//•	Build Angular2->Opens powerPoint
+//Presentation
+//•	Build Dash App->Opens Visual Studio
+//•	             One Never - Ending List
+//•	Available in a dash
+//All Windows 10 Devices
+//•	Always opens scrolled to the bottom.
+//•	Keep your todos ALWAYS in the foreground
+
+
+
+//");
+//        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e) {
+            Item myButton = (Item)((Button)sender).DataContext;
+            StorageFolder myFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            StorageFile toOpen = await myFolder.CreateFileAsync(myButton.FileName, CreationCollisionOption.OpenIfExists);
+
+            await Launcher.LaunchFileAsync(toOpen);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
---
+++ Dash/ViewModels/ItemsViewModel.cs
@@ -0,0 +1,48 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dash.ViewModels {
+    public class ItemsViewModel {
+        public ObservableCollection<Item> ItemCollection { get; set; }
+
+        public ItemsViewModel() {
+            ItemCollection = new ObservableCollection<Item>();
+            AddItemCommand = new RelayCommand(AddNewItem);
+            AddItems();
+        }
+
+        private void AddItems() {
+            List<Item> itemsToAdd = new List<Item>() {
+                new Item() {
+                    Text = "Create Angular2 Architecture Document",
+                    FileName = "Angular2.docx"
+                },
+                new Item() {
+                    Text = "Create Angular2 Presentation for MeetUp",
+                    FileName = "Angular2.pptx"
+                }
+            };
+            foreach (var item in itemsToAdd) {
+                this.ItemCollection.Add(item);
+            }
+        }
+
+        
+
+        public RelayCommand AddItemCommand { get; set; } 
+        private void AddNewItem() {
+            this.ItemCollection.Add(new Item());
+        }
+    }
+
+    public class Item {
+        public bool IsComplete { get; set; } = false;
+        public string Text { get; set; }
+        public string FileName { get; set; }
+    }
+}
